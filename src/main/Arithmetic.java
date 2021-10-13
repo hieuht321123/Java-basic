4c13fd49625e6d3addf464c25123d9665de6a8a1
hieuht <hieuht321123>
2021-10-13 21:31:11 +0700

    java operator

---
+++ src/main/Arithmetic.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double x= 2.5;
+		double y = 12.5;
+		double result ;
+		
+		result = x+y;
+		System.out.println("x + y = " + result);
+	}
+
+}
